80f5167e5f878a616410e7f069916a41cdcb87be
MINJU <mjryu00211@gmail.com>
2025-04-06 00:21:20 +0900

    Fix: 이미지가 업로드되지 않는 오류 수정

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -28,7 +28,8 @@ const wss = setupWebSocket(server);
 
 // 미들웨어
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ extended: true, limit: '20mb' }));
 /*app.use(express.urlencoded({ extended: false })); // 내부 url 파서 사용 */
 
 //포스트 관련 라우트 추가
